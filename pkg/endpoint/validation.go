04c15b2c2dade874cbc1751434636feca44740db
sh-cha <snghuncha@gmail.com>
2025-10-23 17:39:30 +0900

    let comet retry signing when the leader is unavailable

---
+++ pkg/endpoint/validation.go
@@ -0,0 +1,125 @@
+package endpoint
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/cometbft/cometbft/crypto"
+	cryptoenc "github.com/cometbft/cometbft/crypto/encoding"
+	cryptoproto "github.com/cometbft/cometbft/proto/tendermint/crypto"
+	privvalproto "github.com/cometbft/cometbft/proto/tendermint/privval"
+	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	"github.com/cometbft/cometbft/types"
+	"github.com/cosmos/gogoproto/proto"
+)
+
+func ValidationRequestHandler(
+	privVal types.PrivValidator,
+	req privvalproto.Message,
+	chainID string,
+) (privvalproto.Message, error) {
+	var (
+		res privvalproto.Message
+		err error
+	)
+
+	switch r := req.Sum.(type) {
+	case *privvalproto.Message_PubKeyRequest:
+		if r.PubKeyRequest.GetChainId() != chainID {
+			res = mustWrapMsg(&privvalproto.PubKeyResponse{
+				PubKey: cryptoproto.PublicKey{}, Error: &privvalproto.RemoteSignerError{
+					Code: 0, Description: "unable to provide pubkey"}})
+			return res, fmt.Errorf("want chainID: %s, got chainID: %s", r.PubKeyRequest.GetChainId(), chainID)
+		}
+
+		var pubKey crypto.PubKey
+		pubKey, err = privVal.GetPubKey()
+		if err != nil {
+			return res, err
+		}
+		pk, err := cryptoenc.PubKeyToProto(pubKey)
+		if err != nil {
+			res = mustWrapMsg(&privvalproto.PubKeyResponse{
+				PubKey: cryptoproto.PublicKey{}, Error: &privvalproto.RemoteSignerError{Code: 0, Description: err.Error()}})
+		} else {
+			res = mustWrapMsg(&privvalproto.PubKeyResponse{PubKey: pk, Error: nil})
+		}
+
+	case *privvalproto.Message_SignVoteRequest:
+		if r.SignVoteRequest.ChainId != chainID {
+			res = mustWrapMsg(&privvalproto.SignedVoteResponse{
+				Vote: cmtproto.Vote{}, Error: &privvalproto.RemoteSignerError{
+					Code: 0, Description: "unable to sign vote"}})
+			return res, fmt.Errorf("want chainID: %s, got chainID: %s", r.SignVoteRequest.GetChainId(), chainID)
+		}
+
+		vote := r.SignVoteRequest.Vote
+
+		err = privVal.SignVote(chainID, vote)
+		if err != nil && strings.Contains(err.Error(), "lease unavailable") {
+			res = privvalproto.Message{}
+		} else if err != nil {
+			res = mustWrapMsg(&privvalproto.SignedVoteResponse{
+				Vote: cmtproto.Vote{}, Error: &privvalproto.RemoteSignerError{Code: 0, Description: err.Error()}})
+		} else {
+			res = mustWrapMsg(&privvalproto.SignedVoteResponse{Vote: *vote, Error: nil})
+		}
+
+	case *privvalproto.Message_SignProposalRequest:
+		if r.SignProposalRequest.GetChainId() != chainID {
+			res = mustWrapMsg(&privvalproto.SignedProposalResponse{
+				Proposal: cmtproto.Proposal{}, Error: &privvalproto.RemoteSignerError{
+					Code:        0,
+					Description: "unable to sign proposal"}})
+			return res, fmt.Errorf("want chainID: %s, got chainID: %s", r.SignProposalRequest.GetChainId(), chainID)
+		}
+
+		proposal := r.SignProposalRequest.Proposal
+
+		err = privVal.SignProposal(chainID, proposal)
+		if err != nil && strings.Contains(err.Error(), "lease unavailable") {
+			res = privvalproto.Message{}
+		} else if err != nil {
+			res = mustWrapMsg(&privvalproto.SignedProposalResponse{
+				Proposal: cmtproto.Proposal{}, Error: &privvalproto.RemoteSignerError{Code: 0, Description: err.Error()}})
+		} else {
+			res = mustWrapMsg(&privvalproto.SignedProposalResponse{Proposal: *proposal, Error: nil})
+		}
+	case *privvalproto.Message_PingRequest:
+		err, res = nil, mustWrapMsg(&privvalproto.PingResponse{})
+
+	default:
+		err = fmt.Errorf("unknown msg: %v", r)
+	}
+
+	return res, err
+}
+
+func mustWrapMsg(pb proto.Message) privvalproto.Message {
+	msg := privvalproto.Message{}
+
+	switch pb := pb.(type) {
+	case *privvalproto.Message:
+		msg = *pb
+	case *privvalproto.PubKeyRequest:
+		msg.Sum = &privvalproto.Message_PubKeyRequest{PubKeyRequest: pb}
+	case *privvalproto.PubKeyResponse:
+		msg.Sum = &privvalproto.Message_PubKeyResponse{PubKeyResponse: pb}
+	case *privvalproto.SignVoteRequest:
+		msg.Sum = &privvalproto.Message_SignVoteRequest{SignVoteRequest: pb}
+	case *privvalproto.SignedVoteResponse:
+		msg.Sum = &privvalproto.Message_SignedVoteResponse{SignedVoteResponse: pb}
+	case *privvalproto.SignedProposalResponse:
+		msg.Sum = &privvalproto.Message_SignedProposalResponse{SignedProposalResponse: pb}
+	case *privvalproto.SignProposalRequest:
+		msg.Sum = &privvalproto.Message_SignProposalRequest{SignProposalRequest: pb}
+	case *privvalproto.PingRequest:
+		msg.Sum = &privvalproto.Message_PingRequest{PingRequest: pb}
+	case *privvalproto.PingResponse:
+		msg.Sum = &privvalproto.Message_PingResponse{PingResponse: pb}
+	default:
+		panic(fmt.Errorf("unknown message type %T", pb))
+	}
+
+	return msg
+}
